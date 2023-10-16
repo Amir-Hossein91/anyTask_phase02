e521956f3be581519690d8f125f533fab2308936
Amir-Hossein91 <amir_ahmadi9034@yahoo.com>
2023-10-16 20:06:51 +0330

    phase 2: added findByUsername to ManagerRepository

--- src/main/java/com/example/phase_02/repository/ManagerRepository.java
+++ src/main/java/com/example/phase_02/repository/ManagerRepository.java
@@ -4,6 +4,10 @@ import com.example.phase_02.entity.Manager;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ManagerRepository extends JpaRepository<Manager,Long> {
+
+    Optional<Manager> findByUsername (String managerUsername);
 }

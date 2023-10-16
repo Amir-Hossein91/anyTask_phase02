8cebf8157cd20a6f72ba6d12dd5bc38a7599e03b
Amir-Hossein91 <amir_ahmadi9034@yahoo.com>
2023-10-16 20:07:17 +0330

    phase 2: added findByUsername to ManagerService

--- src/main/java/com/example/phase_02/service/ManagerService.java
+++ src/main/java/com/example/phase_02/service/ManagerService.java
@@ -3,9 +3,9 @@ package com.example.phase_02.service;
 import com.example.phase_02.basics.baseService.BaseService;
 import com.example.phase_02.entity.Manager;
 
-import java.util.List;
 
 public interface ManagerService extends BaseService<Manager> {
 
     boolean doesManagerExist();
+    Manager findByUsername(String managerUsername);
 }

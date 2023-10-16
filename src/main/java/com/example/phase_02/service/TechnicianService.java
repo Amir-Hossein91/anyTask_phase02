86e5b53c4a68e96e7a9c67be71b7ffbaa0e84311
Amir-Hossein91 <amir_ahmadi9034@yahoo.com>
2023-10-16 20:12:59 +0330

    phase 2: added findByUsername to TechnicianService

--- src/main/java/com/example/phase_02/service/TechnicianService.java
+++ src/main/java/com/example/phase_02/service/TechnicianService.java
@@ -7,5 +7,7 @@ import java.util.List;
 
 public interface TechnicianService extends BaseService<Technician> {
 
+    Technician findByUsername (String technicianUsername);
+
     List<Technician> saveOrUpdate(List<Technician> technicians);
 }

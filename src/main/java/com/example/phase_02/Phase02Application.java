a39691e5f8f51aac8bdeb2824cc2c68e45e88393
Amir-Hossein91 <amir_ahmadi9034@yahoo.com>
2023-10-17 21:54:46 +0330

    phase 2: Changed @RestController to @Controller

--- src/main/java/com/example/phase_02/Phase02Application.java
+++ src/main/java/com/example/phase_02/Phase02Application.java
@@ -2,10 +2,11 @@ package com.example.phase_02;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+@Controller
 public class  Phase02Application {
 
     public static void main(String[] args) {

5e35c0183d81551b3f0680cd26f778f51e4c4f0b
Amir-Hossein91 <amir_ahmadi9034@yahoo.com>
2023-10-18 21:28:41 +0330

    phase 2: set getter for fields in OrderDTO

--- src/main/java/com/example/phase_02/entity/dto/OrderDTO.java
+++ src/main/java/com/example/phase_02/entity/dto/OrderDTO.java
@@ -3,10 +3,12 @@ package com.example.phase_02.entity.dto;
 import com.example.phase_02.entity.OrderDescription;
 import com.example.phase_02.entity.base.BaseEntity;
 import lombok.Builder;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
 
 @Builder
+@Getter
 public class OrderDTO {
     private long orderId;
     private String subAssistanceTitle;
